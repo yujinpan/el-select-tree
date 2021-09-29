15378af18ab565d87296b2e1aef2bb6669f59dc2
yujinpan <1192878390@qq.com>
2021-09-29 19:29:36 +0800

    Update build scripts

--- rollupx.config.js
+++ rollupx.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   banner:
     '/*!\n' +
-    ` * el-select-tree v${require('../package.json').version}\n` +
+    ` * el-select-tree v${require('./package.json').version}\n` +
     ` * (c) 2019-${new Date().getFullYear()} yujinpan\n` +
     ' * Released under the MIT License.\n' +
     ' */\n',
