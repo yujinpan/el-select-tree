a624cd2a4a4f04ed89f835334f29d8f28f32f878
yujinpan <1192878390@qq.com>
2023-04-18 18:32:28 +0800

    feat: add tests

---
+++ dev/tests/big-data.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <ElSelectTree v-model="value" :data="data"></ElSelectTree>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '',
+      data: Array(100)
+        .fill('')
+        .map((item, index) => ({
+          value: index,
+          label: index,
+          children: Array(100)
+            .fill('')
+            .map((item, index2) => ({
+              value: `${index}-${index2}`,
+              label: `${index}-${index2}`,
+            })),
+        })),
+    };
+  },
+};
+</script>
