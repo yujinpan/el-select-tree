a993ab1e138775cb270eaa0b4ffe4aa8e0e4c7e1
yujinpan <1192878390@qq.com>
2023-04-18 17:29:54 +0800

    chore: use head scripts instead of plugin for Google Analytics

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -1,21 +1,38 @@
-import { defineConfig } from 'vuepress/config';
+import { defineConfig, type HeadTags } from 'vuepress/config';
 
 export default defineConfig({
   base: '/el-select-tree/',
   title: 'el-select-tree',
   description: "ElementUI's el-select combined with el-tree.",
+  head: [...getGAHead()],
   themeConfig: {
     lastUpdated: 'Last Updated',
     repo: 'yujinpan/el-select-tree',
     logo: '/logo.svg',
   },
-  plugins: [
-    'vuepress-plugin-component-demo' as any,
-    [
-      '@vuepress/google-analytics',
-      {
-        ga: 'G-WP35BEJPHM',
-      },
-    ],
-  ],
 });
+
+function getGAHead(): HeadTags {
+  return process.env.NODE_ENV === 'production'
+    ? [
+        [
+          'script',
+          {
+            async: true,
+            src: 'https://www.googletagmanager.com/gtag/js?id=G-WP35BEJPHM',
+          },
+          '',
+        ],
+        [
+          'script',
+          {},
+          `
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-WP35BEJPHM');
+`,
+        ],
+      ]
+    : [];
+}
