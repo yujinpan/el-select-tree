2ffc746d9204a6cabdfd34524c3587b623681d59
yujinpan <1192878390@qq.com>
2023-04-24 20:01:31 +0800

    test: add show-unrender-label.vue

--- dev/App.vue
+++ dev/App.vue
@@ -1,11 +1,17 @@
 <template>
-  <BigDataMultipleSelect />
+  <div>
+    <BigData />
+    <BigDataMultipleSelect />
+    <ShowUnrenderLabel />
+  </div>
 </template>
 
 <script>
 import BigDataMultipleSelect from './tests/big-data-multiple-select.vue';
+import BigData from './tests/big-data.vue';
+import ShowUnrenderLabel from './tests/show-unrender-label.vue';
 
 export default {
-  components: { BigDataMultipleSelect },
+  components: { BigData, ShowUnrenderLabel, BigDataMultipleSelect },
 };
 </script>
---
+++ dev/tests/show-unrender-label.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <ElSelectTree v-model="value" :data="data"></ElSelectTree>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: 2,
+      data: [
+        {
+          value: 1,
+          label: '1-label',
+          children: [{ value: 2, label: '2-label' }],
+        },
+      ],
+    };
+  },
+};
+</script>
