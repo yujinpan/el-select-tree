c725168411cdc826bd28890117c6361410abc82b
yujinpan <1192878390@qq.com>
2024-02-23 18:28:02 +0800

    test: add GitHub issues

---
+++ dev/tests/bug-74.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    use checkbox:
+    <ElSelectTreeVirtual
+      v-model="value"
+      multiple
+      :data="data"
+      check-strictly
+      show-checkbox
+    ></ElSelectTreeVirtual>
+  </div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    const temp = [];
+    for (let i = 0; i < 1000; i++) {
+      temp.push({
+        label: '测试' + i,
+        value: i + '',
+      });
+    }
+    this.data.push(...temp);
+  },
+  methods: {},
+  data() {
+    return {
+      value: '',
+      data: [],
+    };
+  },
+};
+</script>
