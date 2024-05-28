a2f1f1b0dcd611cb61fb6347dbbe2bba5a8370cc
yujinpan <1192878390@qq.com>
2024-05-28 18:28:28 +0800

    chore: add esm/cjs/types modules

--- rollupx.config.js
+++ rollupx.config.js
@@ -8,7 +8,21 @@ module.exports = {
     ' */\n',
   inputFiles: ['**/*'],
   outputDir: 'lib',
-  stylesParseFiles: [],
-  stylesCopyFiles: [],
-  typesOutputDir: 'lib',
+
+  formats: [
+    {
+      format: 'es',
+      inputFiles: ['**/*'],
+      outputDir: 'lib/es',
+      outputFile: '[name][ext]',
+    },
+    {
+      format: 'cjs',
+      inputFiles: ['**/*'],
+      outputDir: 'lib/cjs',
+      outputFile: '[name][ext]',
+    },
+  ],
+
+  typesOutputDir: 'types',
 };
